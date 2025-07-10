494488841092be8faddc8eddab2b0c1cdafce975
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2025-07-10 10:39:10 +0530

    validator pacakge added

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const crypto = require("crypto")
 const urlModel = require("./models/url.js")
 const db = require("./config/config.js")
 const dotenv = require("dotenv")
+const validator = require("validator")
 const PORT = process.env.PORT
 dotenv.config()
 app.use(express.json());
 
 app.post("/shorten",async(req,res)=>{
     const url = req.body.url
+
+    try{
+        if (!validator.isURL(url, { require_protocol: true })) {
+            return res.status(400).json({ message: "Invalid URL format" });
+        }
+    }catch(err){
+         res.status(400).send("issue in validator package")
+    }
+    
+
+
     const geturl = await urlModel.findOne({ Original: url })
 
     if(geturl){
@@ -19,6 +31,7 @@ app.post("/shorten",async(req,res)=>{
 
 
     const short_code = crypto.randomBytes(6).toString('hex');
+
     const new_data= await urlModel.create({
        Original:url,
        shortCode:short_code
